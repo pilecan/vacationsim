f5274425f6e2f772cbd94bfd86c4e8021e47e446
Pierre <checkplane.com@gmail.com>
2018-11-13 22:26:19 -0500

    catch reorder

--- vacationsim/WebContent/menu/js/accordion/setAccordion.js
+++ vacationsim/WebContent/menu/js/accordion/setAccordion.js
@@ -1,13 +1,20 @@
 
-function setAccordionFromSession(galleriesName, CurrentGallery){
+let firstIndex = 0;
+let objJSON = null;
+let mapJSON = [];
+let currentGalleriesName = null;
 
-	let arrayData = [];
+function setAccordionFromSession(galleriesName, currentGallery){
+	currentGalleriesName = galleriesName;
+
 	let line = "";
 
 	let key = galleriesName+"|0";
 	let obj = (window.sessionStorage.getItem(key));
 
-	let objJSON = JSON.parse(new Object(obj));
+	objJSON = JSON.parse(new Object(obj));
+	
+	 firstIndex = 0;
 
 	//document.getElementById("accordion").innerHTML = "";
 
@@ -19,26 +26,33 @@ function setAccordionFromSession(galleriesName, CurrentGallery){
 		if (obj != null){
 			objJSON = JSON.parse(new Object(obj));
 			
-			if (objJSON.gallery == CurrentGallery){
+			if (objJSON.gallery == currentGallery){
 				console.log(galleriesName+"|"+index+objJSON.gallery );
+				
+				if (firstIndex == 0){
+					firstIndex = index;
+				}
+
 				line += "<div class='group'>" +
 						"<h3>"+objJSON.description.split("|")[0]+"</h3>" +
 								"<div>";
 			
 				if (objJSON.media == "image"){
-					data = new Object();
-					line += "title: "+objJSON.description+"<br/>";
-					line += "media: "+objJSON.media+"<br/>";
-					line += "source: "+objJSON.src+"<br/>";
-					line += "description: "+objJSON.description+"<br/>";
+					line += "->'title: "+objJSON.description+"'<br/>";
+					line += "->'media: "+objJSON.media+"'<br/>";
+					line += "->'source: "+objJSON.src+"<br/>'";
+					line += "->'description: "+objJSON.description+"'<br/>";
 				} else {
-					line += "title: "+objJSON.description+"<br/>";
-					line += "media: "+objJSON.media+"<br/>";
-					line += "videoid: "+objJSON.videoid+"<br/>";
-					line += "description: "+objJSON.description+"<br/>";
+					line += "->'title: "+objJSON.description+"'<br/>";
+					line += "->'media: "+objJSON.media+"<br/>'";
+					line += "->'videoid: "+objJSON.videoid+"<br/>'";
+					line += "->'description: "+objJSON.description+"'<br/>";
 				}
 				
 				line += "</div></div>";
+				mapJSON[key] = objJSON;
+				console.log(">>>>>>>>>>>>>>>>>>"+JSON.stringify(mapJSON[key]));
+				
 			}
 			index++;
 
@@ -51,3 +65,25 @@ function setAccordionFromSession(galleriesName, CurrentGallery){
     $( '#accordion' ).accordion( "refresh" );
 
 }
+
+function setNewSession(){
+    $( ".group" ).each(function( index ) {
+    	  console.log( index + ": " + $( this ).text()+"-->"+ $( this ).text().length );
+    	  key = currentGalleriesName+"|"+(index+firstIndex);
+  		  //obj = (window.sessionStorage.getItem(key));
+  		  //objJSON = JSON.parse(new Object(obj));
+  		  //console.log(objJSON.description.split("|")[0]);
+  		  
+  		//var str = "the word you need is 'hello' 'hi' 'bonjour'";
+  		var singleQuoted = $.map($( this ).text().split("'"), function(substr, i) {
+  		   return (i % 2) ? substr : null;
+  		});
+
+  		console.log(singleQuoted.join("\n"));
+  		  
+  		  //window.sessionStorage.setItem(key,));
+
+    });	
+}
+
+
